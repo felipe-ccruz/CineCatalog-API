5b3a4b8fa04feb7464fea726ab563637346a4c66
Felipe Cruz <felp.cruz.comp@gmail.com>
2026-03-02 15:27:04 -0300

    Criação do no Schema para User

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, text, integer, timestamp } from 'drizzle-orm/pg-core'
+import { pgTable, serial, text, integer, timestamp, varchar } from 'drizzle-orm/pg-core'
 
 export const movies = pgTable('movies', {
   id: serial('id').primaryKey(),
@@ -13,3 +13,14 @@ export const movies = pgTable('movies', {
 
 export type Movie = typeof movies.$inferSelect
 export type NewMovie = typeof movies.$inferInsert
+
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  username: text('username').notNull().unique(),
+  email: text('email').notNull().unique(),
+  passwordHash: text('password_hash').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull()
+})
+
+export type User = typeof users.$inferSelect
+export type NewUser = typeof users.$inferInsert
