24dd1cc58769176471ad697d146f4298a331aec2
Felipe Cruz <felp.cruz.comp@gmail.com>
2026-03-03 09:32:02 -0300

    apagandos textos desnecessários em user.schema.ts

--- src/modules/users/user.schema.ts
+++ src/modules/users/user.schema.ts
@@ -1,21 +1,5 @@
 import { t } from 'elysia'
 
-// export const users = pgTable('users', {
-//   id: serial('id').primaryKey(),
-//   username: varchar('username', { length: 255 }).notNull().unique(),
-//   email: varchar('email', { length: 255 }).notNull().unique(),
-//   passwordHash: text('password_hash').notNull(),
-//   createdAt: timestamp('created_at').defaultNow().notNull()
-// })
-
-// export const CreateMovieDTO = t.Object({
-//     title: t.String({ minLength: 1 }),
-//     description: t.Optional(t.String()),
-//     year: t.Number({ minimum: 1888 }),
-//     director: t.String({ minLength: 1 }),
-//     genre: t.String({ minLength: 1 })
-// })
-
 export const CreateUserDTO = t.Object({
     username: t.String({ minLength: 1 }),
     email: t.String({ minLength: 1, format: 'email' }),
